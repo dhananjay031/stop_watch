6181f6153b0306bad1be06fc1e0d66d62b746297
Dhananjay <user@DhananjaysMac.local>
2023-10-05 00:15:28 +0530

    stop watch

---
+++ stopwatch/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import StopWatch from './stopWatch';
+
+
+function App() {
+  return (
+    <div className="App">
+      <StopWatch/>
+    </div>
+  );
+}
+
+export default App;
